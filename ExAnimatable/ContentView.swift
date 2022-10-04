b819710558570adf8e1545fe65a7bcecef82bb7a
jake <palatable7@naver.com>
2022-10-04 12:05:22 +0900

    Implement

--- ExAnimatable/ContentView.swift
+++ ExAnimatable/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  @State var offset = 150.0
+  
+  var body: some View {
+    MyShape2(offsetY: offset)
+      .fill(.blue)
+      .onTapGesture {
+        withAnimation(.easeIn) {
+          offset = Double(Array(50...250).randomElement()!)
+        }
+      }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct MyShape: Shape {
+  func path(in rect: CGRect) -> Path {
+    Path {
+      $0.move(to: CGPoint(x: rect.midX, y: rect.minY))
+      $0.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+      $0.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+      $0.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
     }
+  }
+}
+
+struct MyShape2: Shape {
+  var offsetY: CGFloat
+  
+//  var animatableData: CGFloat {
+//    get { offsetY }
+//    set { offsetY = newValue }
+//  }
+  
+  func path(in rect: CGRect) -> Path {
+    Path {
+      $0.move(to: CGPoint(x: rect.midX, y: rect.minY))
+      $0.addLine(to: CGPoint(x: rect.minX, y: offsetY))
+      $0.addLine(to: CGPoint(x: rect.maxX, y: offsetY))
+      $0.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+    }
+  }
 }
