c03bc94a436a0426bc31e69e029049d042c405fa
jake <palatable7@naver.com>
2022-10-04 11:18:16 +0900

    Initial Commit

---
+++ ExAnimatable/ExAnimatableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExAnimatableApp.swift
+//  ExAnimatable
+//
+//  Created by 김종권 on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ExAnimatableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
